cf459132d89ec26e36e243ff4edf2510fac7fa99
robinhood0107 <pjj4678@gmail.com>
2026-02-04 13:48:33 +0900

    merge 준비

--- client/app/piano-roll/page.tsx
+++
@@ -1,9 +0,0 @@
-import PianoRoll from "@/components/piano-roll/PianoRoll";
-
-export default function PianoRollPage() {
-  return (
-    <div className="w-full h-screen bg-zinc-900 overflow-hidden">
-      <PianoRoll />
-    </div>
-  );
-}
--- client/components/stem-separation/index.ts
+++
@@ -1,6 +0,0 @@
-// Stem Separation 컴포넌트 통합 내보내기
-export { StemSeparationPanel } from "./StemSeparationPanel";
-export { StemDropZone } from "./StemDropZone";
-export { StemTrack } from "./StemTrack";
-export { StemExtractionDialog } from "./StemExtractionDialog";
-export { StemWaveform, StemAudioPlayer } from "./StemAudioPlayer";
